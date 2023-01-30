b11de6347ed64295b85811bb822af9132a14089c
tautologe <tautologe@users.noreply.github.com>
2023-01-30 16:43:35 +0100

    Download GML from Geoportal and convert to Geojson

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O HH_WFS_DB_Rad_Plus.gml https://geodienste.hamburg.de/HH_WFS_DB_Rad_Plus?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&typename=de.hh.up:aktuelles_jahr,de.hh.up:letzte_woche,de.hh.up:jahr2022
+
+ogr2ogr -f "GeoJSON" dbrad2023.json  -s_srs EPSG:25832 -t_srs WGS84 -skipfailures HH_WFS_DB_Rad_Plus.gml aktuelles_jahr
